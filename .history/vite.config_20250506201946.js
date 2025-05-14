a48102e721ff7e1e1c745130c3e69f176e5d5ae9
ErvinClay13 <ervinclay88@gmail.com>
2025-05-14 16:46:12 -0500

    3dr commit

--- .history/vite.config_20250506201946.js
+++ .history/vite.config_20250506201946.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Dj-Dej-Website/',  // 👈 Add this line (use your repo name)
   plugins: [react()],
 })
